a90ee1d911db7b36a61af68a843c2a9d7f910cb6
吴楚 <wuchu@pimengfudembp-2.cmua.dom>
2017-11-08 22:45:35 +1030

    add comments

--- R/HW2_Function_Suburb_and_offence_level_3.R
+++ R/HW2_Function_Suburb_and_offence_level_3.R
@@ -1,25 +1,24 @@
 ## QUESTION ----
 
-#' <Title>
+#' Crime Crime Compare Function
 #'
-#' \code{<compareSuburbs>} <... does what? A short description.>
+#' \code{<compareSuburbs>} <This function draw a plot to compare the crime aomunt of two suburbs.>
 #' @param crime_data A data.table object with the following columns:
 #'     "date" (POSIXct), "suburb" (chr), "postcode" (chr), "offence_level_1" (chr),
 #'     "offence_level_2" (chr), "offence_level_3" (chr), "offence_count" (num).
-#' @param offence_description A character string of <What are your expected inputs?>.
+#' @param offence_description A character string of either "OFFENCES AGAINST PROPERTY" or "OFFENCES AGAINST THE PERSON".
 #' @param suburbs A two-element character vector. Each element is the name (UPPERCASE)
 #'     of an SA suburb.
-#' @import data.table
 #' @export
 #' @return  A ggplot object showing the correlation in offence count between the two input suburbs.
-#' @examples
-#' <one or two examples showing how to use the function>
+#' @examples For example, you can input "OFFENCES AGAINST PROPERTY" as offence_description, and
+#'    "LONSDALE" and "REYNELLA" as suburbs. You will see a ggplot objects showing the trend of crime
+#'    in the two subrubs.
+#'
 compareSuburbs <- function(crime_data, offence_description, suburbs) {
-  print("1")
   require(data.table)
   require(ggplot2)
 
-  print("2")
 
   # Error catching
 
@@ -31,7 +30,6 @@ compareSuburbs <- function(crime_data, offence_description, suburbs) {
   expected_colnames <- c("date", "suburb", "postcode", "offence_level_1", "offence_level_2",
                          "offence_level_3", "offence_count")
 
-  print("3")
 
   if (!all.equal(names(crime_data), expected_colnames)) {
     stop(paste("Input table columns need to match: ",
@@ -44,13 +42,11 @@ compareSuburbs <- function(crime_data, offence_description, suburbs) {
     stop("Fail to find suburb or description!")
   }
 
-  print("4")
   # Make a data table for plotting using data.table transformations
   # You will need to filter, summarise and group by
   # Expect cols: "date", "suburb", "total_offence_count"
   plot_data <- crime_data[suburb %in% suburbs & offence_level_3==offence_description,list(total_offence_count = sum(offence_count)),by=list(suburb,date = month(date))]
 
-  print("5")
   # These lines will transform the plot_data structure to allow us to plot
   # correlations. Try them out
   plot_data[, suburb := plyr::mapvalues(suburb, suburbs, c("x", "y"))]
@@ -59,11 +55,11 @@ compareSuburbs <- function(crime_data, offence_description, suburbs) {
                      fill = 0, value.var = "total_offence_count")
 
   # Generate the plot
-  ggplot(plot_data) +
-    geom_line(aes(date, y),color='steelblue') +
-    geom_line(aes(date, x),color='red') +
-    labs(x = "Time Line (Month)",
-         y = offence_description)
+  ggplot(plot_data, aes(x=date),fill=c(x,y)) +
+    geom_line(aes(y=y,colour=suburb)) +
+    geom_line(aes(y=x,colour=suburb_compare)) +
+    scale_x_continuous(name="Time Line (Month)",breaks=c(1:12)) +
+    labs(y = offence_description)
 }
 
 
