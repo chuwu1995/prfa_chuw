d7a6e506e62870955528dead480477f368e9e375
吴楚 <wuchu@pimengfudembp-2.cmua.dom>
2017-11-08 21:51:45 +1030

    change description

--- R/HW2_Function_Suburb_and_offence_level_3.R
+++ R/HW2_Function_Suburb_and_offence_level_3.R
@@ -15,9 +15,11 @@
 #' @examples
 #' <one or two examples showing how to use the function>
 compareSuburbs <- function(crime_data, offence_description, suburbs) {
+  print("1")
   require(data.table)
   require(ggplot2)
 
+  print("2")
 
   # Error catching
 
@@ -28,6 +30,8 @@ compareSuburbs <- function(crime_data, offence_description, suburbs) {
 
   expected_colnames <- c("date", "suburb", "postcode", "offence_level_1", "offence_level_2",
                          "offence_level_3", "offence_count")
+
+  print("3")
 
   if (!all.equal(names(crime_data), expected_colnames)) {
     stop(paste("Input table columns need to match: ",
@@ -40,11 +44,13 @@ compareSuburbs <- function(crime_data, offence_description, suburbs) {
     stop("Fail to find suburb or description!")
   }
 
+  print("4")
   # Make a data table for plotting using data.table transformations
   # You will need to filter, summarise and group by
   # Expect cols: "date", "suburb", "total_offence_count"
   plot_data <- crime_data[suburb %in% suburbs & offence_level_3==offence_description,list(total_offence_count = sum(offence_count)),by=list(suburb,date = month(date))]
 
+  print("5")
   # These lines will transform the plot_data structure to allow us to plot
   # correlations. Try them out
   plot_data[, suburb := plyr::mapvalues(suburb, suburbs, c("x", "y"))]
